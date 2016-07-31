19a8e46e6ffbbb2f006ff1e844a7c546349f3312
Evan Hahn <me@evanhahn.com>
2016-07-31 10:19:53 -0700

    includeSubDomains is `true` by default
    Closes #10.

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
-// These tests, unlike others, require mocking of requests and responses.
-// Because this header should only be set for HTTPS and that's hard to
-// reliably determine, heuristics are used.
+// Unlike other tests, these require us to mock requests and responses.
+// Because this header should only be set for HTTPS, which is hard to fake
+// reliably, we call the middleware function directly, with fake request and
+// response objects.
 
 var hsts = require('..')
 
@@ -8,116 +9,159 @@ var assert = require('assert')
 var sinon = require('sinon')
 
 describe('hsts', function () {
-  var maxAge = 7776000  // 90 days in seconds
-  var defaultHeader = 'max-age=' + maxAge
-
-  var handler, req, res, next
   beforeEach(function () {
-    handler = hsts({ maxAge: maxAge })
-    req = {}
-    res = { setHeader: sinon.spy() }
-    next = sinon.spy()
+    this.req = { secure: true }
+    this.res = { setHeader: sinon.spy() }
+    this.next = sinon.spy()
   })
 
   it('throws an error with invalid parameters', function () {
-    assert.throws(hsts.bind(this, '1234'))
-    assert.throws(hsts.bind(this, -1234))
-    assert.throws(hsts.bind(this, 1234))
-    assert.throws(hsts.bind(this, 1234, true))
     assert.throws(hsts.bind(this, { maxAge: -123 }))
     assert.throws(hsts.bind(this, { maxAge: '123' }))
     assert.throws(hsts.bind(this, { maxAge: true }))
+    assert.throws(hsts.bind(this, { maxAge: {} }))
+    assert.throws(hsts.bind(this, { maxAge: [] }))
+
+    assert.throws(hsts.bind(this, { maxAge: 1234 }, 'extra argument'))
+
     assert.throws(hsts.bind(this, { setIf: 123 }))
     assert.throws(hsts.bind(this, { setIf: true }))
-    assert.throws(hsts.bind(this, { setIf: function () {}, force: true }))
+
+    assert.throws(hsts.bind(this, { maxage: false }))
     assert.throws(hsts.bind(this, { maxage: 1234 }))
+    assert.throws(hsts.bind(this, {
+      includeSubDomains: true,
+      includeSubdomains: true
+    }))
+    assert.throws(hsts.bind(this, {
+      includeSubDomains: false,
+      includeSubdomains: true
+    }))
+    assert.throws(hsts.bind(this, { setIf: function () {}, force: true }))
   })
 
-  it('sets a default value to 1 day', function () {
-    handler = hsts()
-    req.secure = true
-    handler(req, res, next)
-    assert(res.setHeader.calledWith('Strict-Transport-Security', 'max-age=86400'))
+  it('sets no header if req.secure is false', function () {
+    this.req.secure = false
+
+    hsts()(this.req, this.res, this.next)
+
+    assert(this.next.calledOnce)
+    assert(this.next.calledWithExactly())
+    assert(!this.res.setHeader.called)
   })
 
-  it('can include subdomains with no specified max-age', function () {
-    handler = hsts({ includeSubdomains: true })
-    req.secure = true
-    handler(req, res, next)
-    assert(res.setHeader.calledWith('Strict-Transport-Security', 'max-age=86400; includeSubDomains'))
+  it('by default, sets max-age to 1 day and includeSubDomains', function () {
+    hsts()(this.req, this.res, this.next)
+
+    assert(this.next.calledOnce)
+    assert(this.next.calledWithExactly())
+    assert(this.res.setHeader.calledWithExactly('Strict-Transport-Security', 'max-age=86400; includeSubDomains'))
   })
 
-  it('can include subdomains and preload with no specified max-age', function () {
-    handler = hsts({ includeSubdomains: true, preload: true })
-    req.secure = true
-    handler(req, res, next)
-    assert(res.setHeader.calledWith('Strict-Transport-Security', 'max-age=86400; includeSubDomains; preload'))
+  it('can set max-age to a positive integer', function () {
+    hsts({
+      maxAge: 1234
+    })(this.req, this.res, this.next)
+
+    assert(this.next.calledOnce)
+    assert(this.next.calledWithExactly())
+    assert(this.res.setHeader.calledWithExactly('Strict-Transport-Security', 'max-age=1234; includeSubDomains'))
   })
 
-  it('is unset if req.secure is false', function () {
-    req.secure = false
-    handler(req, res, next)
-    assert(!res.setHeader.called)
+  it('rounds the max-age', function () {
+    hsts({
+      maxAge: 1234.56
+    })(this.req, this.res, this.next)
+
+    assert(this.next.calledOnce)
+    assert(this.next.calledWithExactly())
+    assert(this.res.setHeader.calledWithExactly('Strict-Transport-Security', 'max-age=1235; includeSubDomains'))
   })
 
-  it('is set if req.secure is true', function () {
-    req.secure = true
-    handler(req, res, next)
-    assert(res.setHeader.calledWith('Strict-Transport-Security', defaultHeader))
+  it('can set max-age to -0', function () {
+    hsts({
+      maxAge: -0
+    })(this.req, this.res, this.next)
+
+    assert(this.next.calledOnce)
+    assert(this.next.calledWithExactly())
+    assert(this.res.setHeader.calledWithExactly('Strict-Transport-Security', 'max-age=0; includeSubDomains'))
   })
 
-  it('is always set if forced', function () {
-    handler = hsts({ maxAge: maxAge, force: true })
-    req.secure = false
-    handler(req, res, next)
-    assert(res.setHeader.calledWith('Strict-Transport-Security', defaultHeader))
+  it('can set max-age to 0', function () {
+    hsts({
+      maxAge: 0
+    })(this.req, this.res, this.next)
+
+    assert(this.next.calledOnce)
+    assert(this.next.calledWithExactly())
+    assert(this.res.setHeader.calledWithExactly('Strict-Transport-Security', 'max-age=0; includeSubDomains'))
   })
 
-  it('can be set to -0', function () {
-    req.secure = true
-    handler = hsts({ maxAge: -0 })
-    handler(req, res, next)
-    assert(res.setHeader.calledWith('Strict-Transport-Security', 'max-age=0'))
+  it('can disable subdomains with the includeSubDomains option', function () {
+    hsts({
+      includeSubDomains: false
+    })(this.req, this.res, this.next)
+
+    assert(this.next.calledOnce)
+    assert(this.next.calledWithExactly())
+    assert(this.res.setHeader.calledWithExactly('Strict-Transport-Security', 'max-age=86400'))
   })
 
-  it('can be set to 0', function () {
-    req.secure = true
-    handler = hsts({ maxAge: 0 })
-    handler(req, res, next)
-    assert(res.setHeader.calledWith('Strict-Transport-Security', 'max-age=0'))
+  it('can disable subdomains with the includeSubdomains option', function () {
+    hsts({
+      includeSubdomains: false
+    })(this.req, this.res, this.next)
+
+    assert(this.next.calledOnce)
+    assert(this.next.calledWithExactly())
+    assert(this.res.setHeader.calledWithExactly('Strict-Transport-Security', 'max-age=86400'))
   })
 
-  it('can include subdomains using includeSubdomains option', function () {
-    var expectedHeader = defaultHeader + '; includeSubDomains'
-    req.secure = true
-    handler = hsts({ maxAge: maxAge, includeSubdomains: true })
-    handler(req, res, next)
-    assert(res.setHeader.calledWith('Strict-Transport-Security', expectedHeader))
+  it('can enable preloading', function () {
+    hsts({
+      preload: true
+    })(this.req, this.res, this.next)
+
+    assert(this.next.calledOnce)
+    assert(this.next.calledWithExactly())
+    assert(this.res.setHeader.calledWithExactly('Strict-Transport-Security', 'max-age=86400; includeSubDomains; preload'))
   })
 
-  it('can include subdomains using preferred includeSubDomains option', function () {
-    var expectedHeader = defaultHeader + '; includeSubDomains'
-    req.secure = true
-    handler = hsts({ maxAge: maxAge, includeSubdomains: false, includeSubDomains: true })
-    handler(req, res, next)
-    assert(res.setHeader.calledWith('Strict-Transport-Security', expectedHeader))
-  })
-
-  it('lets you decide whether it should be set', function () {
-    handler = hsts({
-      maxAge: maxAge,
+  it('can set the header based on your own condition', function () {
+    var options = {
       setIf: function (req) {
         return req.pleaseSet
       }
-    })
-    handler(req, res, next)
-    req.pleaseSet = true
-    handler(req, res, next)
-    assert(res.setHeader.calledOnce)
+    }
+
+    hsts(options)({}, this.res, this.next)
+
+    assert(this.next.calledOnce)
+    assert(this.next.calledWithExactly())
+    assert(!this.res.setHeader.called)
+
+    hsts(options)({ pleaseSet: true }, this.res, this.next)
+
+    assert(this.next.calledTwice)
+    assert(this.next.alwaysCalledWithExactly())
+    assert(this.res.setHeader.calledWithExactly('Strict-Transport-Security', 'max-age=86400; includeSubDomains'))
+  })
+
+  it('can force the header', function () {
+    this.req.secure = false
+
+    hsts({
+      force: true
+    })(this.req, this.res, this.next)
+
+    assert(this.next.calledOnce)
+    assert(this.next.calledWithExactly())
+    assert(this.res.setHeader.calledWithExactly('Strict-Transport-Security', 'max-age=86400; includeSubDomains'))
   })
 
   it('names its function and middleware', function () {
     assert.equal(hsts.name, 'hsts')
-    assert.equal(hsts({ maxAge: 1 }).name, 'hsts')
+    assert.equal(hsts().name, 'hsts')
   })
 })
