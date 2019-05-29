530c10f1efd69c2c0ad3d2ee63078b521ccf9a95
George Zografos <george.zografos@dazn.com>
2019-05-30 01:42:43 +0200

    Update tests

--- test/index.js
+++ test/index.js
@@ -24,21 +24,8 @@ describe('hsts', function () {
 
     assert.throws(hsts.bind(this, { maxAge: 1234 }, 'extra argument'))
 
-    assert.throws(hsts.bind(this, { setIf: 123 }))
-    assert.throws(hsts.bind(this, { setIf: true }))
-    assert.throws(hsts.bind(this, { setIf: false }))
-    assert.throws(hsts.bind(this, { setIf: null }))
-
     assert.throws(hsts.bind(this, { maxage: false }))
     assert.throws(hsts.bind(this, { maxage: 1234 }))
-    assert.throws(hsts.bind(this, {
-      includeSubDomains: true,
-      includeSubdomains: true
-    }))
-    assert.throws(hsts.bind(this, {
-      includeSubDomains: false,
-      includeSubdomains: true
-    }))
   })
 
   it('by default, sets max-age to 180 days and adds "includeSubDomains"', function () {
@@ -95,26 +82,6 @@ describe('hsts', function () {
       .expect('Strict-Transport-Security', 'max-age=15552000')
   })
 
-  it('can disable subdomains with the includeSubdomains option, but a deprecation warning is shown', function () {
-    // We can remove this test in hsts@3.
-    const deprecationPromise = new Promise(resolve => {
-      process.on('deprecation', (deprecationError) => {
-        if (deprecationError.message.includes('The "includeSubdomains" parameter is deprecated. Use "includeSubDomains" (with a capital D) instead.')) {
-          resolve()
-        }
-      })
-    })
-
-    const supertestPromise = request(app({
-      includeSubdomains: false
-    }))
-      .get('/')
-      .expect(200)
-      .expect('Strict-Transport-Security', 'max-age=15552000')
-
-    return Promise.all([deprecationPromise, supertestPromise])
-  })
-
   it('can enable preloading', function () {
     return request(app({
       preload: true
@@ -122,43 +89,6 @@ describe('hsts', function () {
       .get('/')
       .expect(200)
       .expect('Strict-Transport-Security', 'max-age=15552000; includeSubDomains; preload')
-  })
-
-  it('can use setIf to conditionally set the header, but it is deprecated', function () {
-    // We can remove this test in hsts@3.
-    const deprecationPromise = new Promise(resolve => {
-      process.on('deprecation', (deprecationError) => {
-        if (deprecationError.message.includes('The "setIf" parameter is deprecated. Refer to the documentation to see how to set the header conditionally.')) {
-          resolve()
-        }
-      })
-    })
-
-    const server = app({
-      setIf: function (req) {
-        return req.headers['x-should-set'] === 'yes'
-      }
-    })
-
-    const shouldntSetPromise = request(server)
-      .get('/')
-      .set('X-Should-Set', 'no')
-      .expect(200)
-      .then(function (res) {
-        assert(!('strict-transport-security' in res.headers))
-      })
-
-    const shouldSetPromise = request(server)
-      .get('/')
-      .set('X-Should-Set', 'yes')
-      .expect(200)
-      .expect('Strict-Transport-Security', 'max-age=15552000; includeSubDomains')
-
-    return Promise.all([
-      deprecationPromise,
-      shouldntSetPromise,
-      shouldSetPromise
-    ])
   })
 
   it('does nothing with the `force` option; allowed for backwards compatibility', function () {
